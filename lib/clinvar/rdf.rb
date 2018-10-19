32372986740436d47e6ac188a52b20ef82237536
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-19 21:36:34 +0900

    add documentation comments

--- lib/clinvar/rdf.rb
+++ lib/clinvar/rdf.rb
@@ -1,12 +1,17 @@
 require 'clinvar/rdf/version'
 
+# Namespace for ClinVar
 module ClinVar
+
+  # Namespace for ClinVar::RDF
   module RDF
     autoload :Model, 'clinvar/rdf/model'
     autoload :ModelHelper, 'clinvar/rdf/model_helper'
   end
 
+  # Namespace for ClinVar::XML
   module XML
     autoload :XSD, 'clinvar/xml/xsd'
   end
+
 end
--- lib/clinvar/rdf/model_helper.rb
+++ lib/clinvar/rdf/model_helper.rb
@@ -1,8 +1,15 @@
 require 'rdf'
 
+# Namespace for ClinVar
 module ClinVar
+
+  # Namespace for ClinVar::RDF
   module RDF
+
+    # Module for helping model classes
     module ModelHelper
+
+      # Module to define class method for ClinVar::RDF::ModelHelper
       module ClassMethods
         def build(*args)
           new
