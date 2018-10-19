c44fb53964a6a83f72bcf6f83bbf15fae0dc799c
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-19 18:02:42 +0900

    add specs for .open and impl.

--- lib/clinvar/xsd.rb
+++ lib/clinvar/xsd.rb
@@ -1,5 +1,14 @@
 module ClinVar
   class XSD
+    class << self
+      def open(path)
+        xsd = new
+
+        return xsd unless block_given?
+
+        yield xsd
+      end
+    end
   end
 end
 
--- spec/xsd_spec.rb
+++ spec/xsd_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 RSpec.describe ClinVar::XSD do
+
+  let (:xsd_sample) { File.expand_path('../../sample/variation_archive_20180705.xsd', __FILE__) }
+
+  it 'should respond to .open' do
+    expect(ClinVar::XSD.respond_to?(:open)).to be_truthy
+  end
+
+  describe '.open' do
+
+    it 'should return an instance of self' do
+      expect(ClinVar::XSD.open(xsd_sample)).kind_of? ClinVar::XSD
+    end
+
+    context 'if block given' do
+
+      it 'should return last evaluation of given block' do
+        actual = ClinVar::XSD.open(xsd_sample) do
+          'last evaluation'
+        end
+
+        expect(actual).to eq('last evaluation')
+      end
+
+    end
+
+  end
+
 end
