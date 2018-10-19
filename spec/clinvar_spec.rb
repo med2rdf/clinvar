bee6a7b8e288ff789efa9f883c9c4784907ec671
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-19 16:37:45 +0900

    pass first spec

--- spec/clinvar_spec.rb
+++ spec/clinvar_spec.rb
@@ -1,9 +1,7 @@
+require 'spec_helper'
+
 RSpec.describe Clinvar do
   it 'has a version number' do
     expect(Clinvar::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
