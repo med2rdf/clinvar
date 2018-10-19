c78409fd01f6c579723b859fda939f23b15dfe77
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-19 20:17:04 +0900

    add apecs for ClinVar::RDF::Model::VariationArchiveType

--- lib/clinvar/rdf/model_helper.rb
+++ lib/clinvar/rdf/model_helper.rb
@@ -1,6 +1,16 @@
 module ClinVar
   module RDF
     module ModelHelper
+      module ClassMethods
+        def build(*args)
+          new
+        end
+      end
+
+      def self.included(klass)
+        klass.extend ClassMethods
+      end
+
       def to_rdf
 
       end
--- spec/rdf/model_spec.rb
+++ spec/rdf/model_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe ClinVar::RDF::Model do
 
   end
 
+  describe 'ClinVar::RDF::Model::VariationArchiveType' do
+
+    it 'should respond to .build' do
+      expect(ClinVar::RDF::Model::VariationArchiveType.respond_to?(:build)).to be_truthy
+    end
+
+    describe '.build' do
+
+      it 'should return an instance of self' do
+        expect(ClinVar::RDF::Model::VariationArchiveType.build({})).to be_kind_of(ClinVar::RDF::Model::VariationArchiveType)
+      end
+
+    end
+
+    it 'should respond to #to_rdf' do
+      expect(ClinVar::RDF::Model::VariationArchiveType.new.respond_to?(:to_rdf)).to be_truthy
+    end
+
+  end
+
 end
