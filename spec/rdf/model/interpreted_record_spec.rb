0863cefaa668cbdb499e25bfb0e70b8df0829c45
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-22 19:34:07 +0900

    add specs for method response

--- spec/rdf/model/interpreted_record_spec.rb
+++ spec/rdf/model/interpreted_record_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 RSpec.describe ClinVar::RDF::Model::InterpretedRecord do
 
+  it 'should respond to .build' do
+    expect(ClinVar::RDF::Model::InterpretedRecord.respond_to?(:build)).to be_truthy
+  end
+
+  describe '.build' do
+
+    it 'should return an instance of self' do
+      expect(ClinVar::RDF::Model::InterpretedRecord.build({})).to be_kind_of(ClinVar::RDF::Model::InterpretedRecord)
+    end
+
+  end
+
+  it 'should respond to #to_rdf' do
+    expect(ClinVar::RDF::Model::InterpretedRecord.new.respond_to?(:to_rdf)).to be_truthy
+  end
+
+  describe '#to_rdf' do
+
+    it 'should return an instance of RDF::Graph' do
+      expect(ClinVar::RDF::Model::InterpretedRecord.new.to_rdf).to be_kind_of(RDF::Graph)
+    end
+
+  end
+
 end
