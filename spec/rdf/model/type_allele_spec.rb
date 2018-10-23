2df43e579748f799370c5847aa6164de95dc14ff
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-23 11:30:35 +0900

    add type assertion

--- spec/rdf/model/type_allele_spec.rb
+++ spec/rdf/model/type_allele_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'xmlsimple'
 
 RSpec.describe ClinVar::RDF::Model::TypeAllele do
 
@@ -22,6 +23,32 @@ RSpec.describe ClinVar::RDF::Model::TypeAllele do
 
     it 'should return an instance of RDF::Graph' do
       expect(ClinVar::RDF::Model::TypeAllele.new.to_rdf).to be_kind_of(RDF::Graph)
+    end
+
+  end
+
+  describe 'RDF::Graph' do
+
+    context 'variation_archive_20180705.xsd' do
+
+      before :all do
+        record = XmlSimple.xml_in(File.read('spec/sample/VCV000018390.xml'))
+
+        @subject = RDF::URI.new('http://example.com/VCV000018390/InterpretedRecord/SimpleAllele')
+
+        @graph = ClinVar::RDF::Model::TypeAllele
+                   .build(record['InterpretedRecord'][0]['SimpleAllele'][0])
+                   .subject(@subject)
+                   .to_rdf
+      end
+
+      let (:subject) { @subject }
+      let (:g) { @graph }
+
+      it 'should be TypeAllele' do
+        expect(g.query(subject: subject, predicate: RDF.type).first_object).to eq(ClinVar::RDF::Vocab[:TypeAllele])
+      end
+
     end
 
   end
