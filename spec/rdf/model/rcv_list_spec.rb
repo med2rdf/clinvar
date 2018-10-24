2d7e93383a5e2069e20a81fccc1d05756284f5a5
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-24 17:52:52 +0900

    add type assertion

--- spec/rdf/model/rcv_list_spec.rb
+++ spec/rdf/model/rcv_list_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'xmlsimple'
 
 RSpec.describe ClinVar::RDF::Model::InterpretedRecord::RCVList do
 
@@ -22,6 +23,32 @@ RSpec.describe ClinVar::RDF::Model::InterpretedRecord::RCVList do
 
     it 'should return an instance of RDF::Graph' do
       expect(ClinVar::RDF::Model::InterpretedRecord::RCVList.new.to_rdf).to be_kind_of(RDF::Graph)
+    end
+
+  end
+
+  describe 'RDF::Graph' do
+
+    context 'variation_archive_20180705.xsd' do
+
+      before :all do
+        record = XmlSimple.xml_in(File.read('spec/sample/VCV000018390.xml'))
+
+        @subject = RDF::URI.new('http://example.com/VCV000018390/InterpretedRecord/RCVList')
+
+        @graph = ClinVar::RDF::Model::InterpretedRecord::RCVList
+                   .build(record['InterpretedRecord'][0]['RCVList'][0])
+                   .subject(@subject)
+                   .to_rdf
+      end
+
+      let (:subject) { @subject }
+      let (:g) { @graph }
+
+      it 'should be RCVList' do
+        expect(g.query(subject: subject, predicate: RDF.type).first_object).to eq(ClinVar::RDF::Vocab[:RCVList])
+      end
+
     end
 
   end
