0f1accb611215bebfa2b2b9b4ef3d1b0e157a6bb
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-24 16:35:46 +0900

    add specs for methods response

--- spec/rdf/model/rcv_list_spec.rb
+++ spec/rdf/model/rcv_list_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 RSpec.describe ClinVar::RDF::Model::InterpretedRecord::RCVList do
 
+  it 'should respond to .build' do
+    expect(ClinVar::RDF::Model::InterpretedRecord::RCVList.respond_to?(:build)).to be_truthy
+  end
+
+  describe '.build' do
+
+    it 'should return an instance of self' do
+      expect(ClinVar::RDF::Model::InterpretedRecord::RCVList.build({})).to be_kind_of(ClinVar::RDF::Model::InterpretedRecord::RCVList)
+    end
+
+  end
+
+  it 'should respond to #to_rdf' do
+    expect(ClinVar::RDF::Model::InterpretedRecord::RCVList.new.respond_to?(:to_rdf)).to be_truthy
+  end
+
+  describe '#to_rdf' do
+
+    it 'should return an instance of RDF::Graph' do
+      expect(ClinVar::RDF::Model::InterpretedRecord::RCVList.new.to_rdf).to be_kind_of(RDF::Graph)
+    end
+
+  end
+
 end
