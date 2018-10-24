83e851e870d0f67735ae5012755d7e7ec4c48c72
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-24 18:42:23 +0900

    add spec for a RCV Accession

--- spec/rdf/model/rcv_list_spec.rb
+++ spec/rdf/model/rcv_list_spec.rb
@@ -40,6 +40,13 @@ RSpec.describe ClinVar::RDF::Model::InterpretedRecord::RCVList do
                    .build(record['InterpretedRecord'][0]['RCVList'][0])
                    .subject(@subject)
                    .to_rdf
+
+        @subject_rcv = RDF::URI.new('http://example.com/VCV000018390/InterpretedRecord/RCVList/RCVAccession')
+
+        @graph_rcv = ClinVar::RDF::Model::TypeRCV
+                       .build(record['InterpretedRecord'][0]['RCVList'][0]['RCVAccession'].find { |x| x['Accession'] == 'RCV000020058' })
+                       .subject(@subject_rcv)
+                       .to_rdf
       end
 
       let (:subject) { @subject }
@@ -58,6 +65,37 @@ RSpec.describe ClinVar::RDF::Model::InterpretedRecord::RCVList do
         klass = result.select { |x| x.is_a?(RDF::Node) }.map { |s| g.query(subject: s, predicate: RDF.type).map(&:object) }.flatten.uniq
         expect(klass.size).to be(1)
         expect(klass.first).to eq(ClinVar::RDF::Vocab[:TypeRCV])
+      end
+
+      context 'RCV000020058' do
+
+        let (:subject) { @subject_rcv }
+        let (:g) { @graph_rcv }
+
+        it 'should be TypeRCV' do
+          expect(g.query(subject: subject, predicate: RDF.type).first_object).to eq(ClinVar::RDF::Vocab[:TypeRCV])
+        end
+
+        # attribute / string
+        it 'should have title' do
+          expect(g.query(subject: subject, predicate: ClinVar::RDF::Vocab[:title]).first_object).to eq(RDF::Literal.new('NM_000690.3(ALDH2):c.1510G>A (p.Glu504Lys) AND Acute alcohol sensitivity'))
+        end
+
+        # attribute / date
+        it 'should have date last evaluated' do
+          expect(g.query(subject: subject, predicate: ClinVar::RDF::Vocab[:date_last_evaluated]).first_object).to eq(RDF::Literal::Date.new('2013-04-04'))
+        end
+
+        # element / array type
+        it 'should have interpreted conditions' do
+          result = g.query(subject: subject, predicate: ClinVar::RDF::Vocab[:interpreted_condition])
+          expect(result.size).to be(1)
+
+          expect(g.query(subject: result.first.object, predicate: ClinVar::RDF::Vocab[:db]).first_object).to eq(RDF::Literal.new('MedGen'))
+          expect(g.query(subject: result.first.object, predicate: ClinVar::RDF::Vocab[:id]).first_object).to eq(RDF::Literal.new('C2674838'))
+          expect(g.query(subject: result.first.object, predicate: ClinVar::RDF::Vocab[:type_rcv_interpreted_condition]).first_object).to eq(RDF::Literal.new('Acute alcohol sensitivity'))
+        end
+
       end
 
     end
