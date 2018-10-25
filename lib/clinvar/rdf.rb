a6ac16ebcdf70a0132f1df3321ce3855fee2d992
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-25 16:06:05 +0900

    remove model.rb and evaluate model on required

--- lib/clinvar/rdf.rb
+++ lib/clinvar/rdf.rb
@@ -5,7 +5,6 @@ module ClinVar
 
   # Namespace for ClinVar::RDF
   module RDF
-    autoload :Model, 'clinvar/rdf/model'
     autoload :ModelHelper, 'clinvar/rdf/model_helper'
     autoload :Vocab, 'clinvar/rdf/vocab'
   end
@@ -16,3 +15,11 @@ module ClinVar
   end
 
 end
+
+# load data model dynamically
+root  = File.expand_path('../../../', __FILE__)
+files = Dir[File.join(root, '*.xsd')]
+raise("File *.xsd does not found in #{root}") if files.empty?
+
+model = ClinVar::XML::XSD.open(files.last) { |xsd| xsd.to_ruby }
+eval(model)
