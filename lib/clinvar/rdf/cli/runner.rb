a0561d7b91e3b9267221e4c981e19ecf2a9c9e97
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-26 10:39:47 +0900

    initial import

---
+++ lib/clinvar/rdf/cli/runner.rb
@@ -0,0 +1,62 @@
+require 'active_support'
+require 'active_support/core_ext/object/inclusion'
+require 'active_support/core_ext/string/strip'
+require 'active_support/inflector'
+
+module ClinVar
+  module RDF
+    module CLI
+
+      PROG_NAME = 'clinvar-rdf'.freeze
+
+      class Runner
+        def run
+          command = ARGV.shift || '--help'
+
+          case command
+          when '-v', '--version'
+            STDERR.puts ClinVar::RDF::VERSION
+          when '-h', '--help'
+            STDERR.puts help
+          when *commands
+            target = ClinVar::RDF::CLI.const_get(command.capitalize).new
+            target.run if target.respond_to?(:run)
+          else
+            STDERR.puts "Unknown command: '#{command}'"
+            STDERR.puts
+            STDERR.puts help
+            exit 1
+          end
+
+          exit 0
+        end
+
+        private
+
+        def commands
+          klasses = ClinVar::RDF::CLI.constants.reject do |c|
+            c.in? %i[Runner PROG_NAME]
+          end
+          klasses.map { |k| k.to_s.underscore }
+        end
+
+        def help
+          <<-USAGE.strip_heredoc % commands.map { |c| "    #{c}" }.join("\n")
+          Usage: #{PROG_NAME} [command] [options] [arguments]
+  
+          RDF Converter for ClinVar
+  
+          Commands:
+          %s
+  
+          Options:
+              -h, --help                       show help
+              -v, --version                    print version
+  
+          Run '#{PROG_NAME} COMMAND --help' for more information on a command
+          USAGE
+        end
+      end
+    end
+  end
+end
