6416ab0bf8eb8b6087b1c0461e55d4e23c6efd18
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-26 15:23:51 +0900

    add prefixes

--- lib/clinvar/rdf/vocab.rb
+++ lib/clinvar/rdf/vocab.rb
@@ -2,6 +2,17 @@ require 'rdf'
 
 module ClinVar
   module RDF
+
+    RCV_ID_BASE       = 'http://identifiers.org/clinvar.record:'.freeze
+    SCV_ID_BASE       = 'http://identifiers.org/clinvar.submission:'.freeze
+    VCV_ID_BASE       = 'http://identifiers.org/clinvar:'.freeze
+    HGNC_ID_BASE      = 'http://identifiers.org/hgnc/'.freeze
+    NCBI_GENE_ID_BASE = 'http://identifiers.org/ncbigene/'.freeze
+    REFSEQ_ID_BASE    = 'http://identifiers.org/refseq/'.freeze
+
+    FALDO = ::RDF::Vocabulary.new('http://biohackathon.org/resource/faldo#')
+    HCO   = ::RDF::Vocabulary.new('http://identifiers.org/hco/')
+
     class Vocab < ::RDF::Vocabulary('http://purl.jp/bio/10/clinvar/')
     end
   end
