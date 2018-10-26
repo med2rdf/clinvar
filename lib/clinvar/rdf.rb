05803980eccab8dbea495e9487e83b914b5c5ea6
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-10-26 15:40:22 +0900

    autoload modules on required

--- lib/clinvar/rdf.rb
+++ lib/clinvar/rdf.rb
@@ -5,12 +5,25 @@ module ClinVar
 
   # Namespace for ClinVar::RDF
   module RDF
+    require 'clinvar/rdf/vocab'
+
+    # Namespace for ClinVar::RDF::CLI
+    module CLI
+      autoload :Convert, 'clinvar/rdf/cli/convert'
+      autoload :Runner, 'clinvar/rdf/cli/runner'
+    end
+
+    # Namespace for ClinVar::RDF::Turtle
+    module Turtle
+      autoload :Writer, 'clinvar/rdf/turtle/writer'
+    end
+
     autoload :ModelHelper, 'clinvar/rdf/model_helper'
-    autoload :Vocab, 'clinvar/rdf/vocab'
   end
 
   # Namespace for ClinVar::XML
   module XML
+    autoload :Reader, 'clinvar/xml/reader'
     autoload :XSD, 'clinvar/xml/xsd'
   end
 
